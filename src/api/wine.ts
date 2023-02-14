5598fe5ac67d052db38fc4bd1ebaa4b864cecf41
eniolajayi <ajayieniola16@gmail.com>
2023-02-14 10:49:40 +0100

    update getWineRecommendation function

--- src/api/wine.ts
+++ src/api/wine.ts
@@ -3,13 +3,21 @@ type WineRecommendationParams = {
     maxPrice: number;
     minRating: number;
     count: number;
-  }
-  
-  async function getWineRecommendation({wineName, maxPrice, minRating, count}:WineRecommendationParams ){
-    let response =  await fetch(`https://api.spoonacular.com/food/wine/recommendation?wine=${wineName}&maxPrice=${maxPrice}&minRating=${minRating}&number=${count}`);
-  
-    return response.json();
-  
-  }
+}
 
-export {getWineRecommendation}
+async function getWineRecommendation({ wineName, maxPrice, minRating, count }: WineRecommendationParams) {
+    let options: RequestInit = {
+        method: 'GET',
+        headers: {
+            "x-api-key": import.meta.env.API_KEY,
+        },
+        redirect: 'follow',
+    };
+    let response = await fetch(`https://api.spoonacular.com/food/wine/recommendation?wine=${wineName}&maxPrice=${maxPrice}&minRating=${minRating}&number=${count}`, options);
+
+    return response.json();
+
+}
+
+
+export { getWineRecommendation }
