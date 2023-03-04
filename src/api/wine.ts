4d26e2bd58c20f562cc8dfd52fa5c14e91d33a1f
eniolajayi <ajayieniola16@gmail.com>
2023-03-04 03:14:22 +0100

    update get wine recommendation hook

--- src/api/wine.ts
+++ src/api/wine.ts
@@ -19,7 +19,10 @@ async function getWineRecommendation({ wineName, maxPrice, minRating, count }: W
 // TODO Set env variable for base url
 const BASE_URL = "https://api.spoonacular.com/";
 
+//{{baseUrl}}/food/wine/recommendation?wine=merlot&maxPrice=50&minRating=0.7&number=3
+export const useGetWineRecommendation = ({ wine, maxPrice, minRating, count }: WineRecommendationParams) => {
+    // TODO Clean up URL
+    const result = useSWR(`https://api.spoonacular.com/food/wine/recommendation?wine=${wine}&maxPrice=${maxPrice}&minRating=${minRating}&number=${count}`,
+        (url) => fetcher(url));
+    return result as SWRResponse<WineRecommendationData, WineRecommendationError>;
 }
-
-
-export { getWineRecommendation }
