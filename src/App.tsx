7e581da6fcb44a55e3b0ae33798178421772c6fc
eniolajayi <ajayieniola16@gmail.com>
2023-03-04 03:00:40 +0100

    create simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useGetWineRecommendation, Wine, WineRecommendationParams } from "./api/wine";
 import wineCategories from "./wine-categories";
 
+
 export default function App() {
+  const params: WineRecommendationParams = {
+    wine: "merlot",
+    maxPrice: 50,
+    minRating: 0.7,
+    count: 3,
+  }
+  let result: Wine[] = [];
+
+  const { data, error, isLoading } = useGetWineRecommendation(params);
+  if (data) {
+    result = data.recommendedWines;
+  }
+
   return (
     <>
       <div className="navbar bg-base-100">
@@ -9,11 +24,29 @@ export default function App() {
         </a>
       </div>
       <ul>
-      {wineCategories.wines.map((wine, idx) => {
-        return <li key={idx} className="">{wine.name}</li>;
-      })}
+        {wineCategories.wines.map((wine, idx) => {
+          return <li key={idx} className="">{wine.name}</li>;
+        })}
       </ul>
-      
+      <div>
+        <div>
+          {result.map((wine: Wine) => {
+            return <div key={wine.id}>
+              <img src={wine.imageUrl} alt="" />
+              <h2>{wine.title}</h2>
+              <p>{wine.description}</p>
+              <div>
+                <span>{wine.averageRating}</span><span>{wine.ratingCount}</span>
+              </div>
+              <div>{wine.price}</div>
+
+
+              <a href={wine.link}>Get</a>
+            </div>
+          })}
+        </div>
+      </div>
+
       <button className="btn btn-primary w-64 rounded-full">Get Started</button>
     </>
   );
